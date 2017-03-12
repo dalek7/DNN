fb0b7d19e27f9cf681d50b018139867b70bcb270
dalek7 <dalek@kaist.ac.kr>
2017-03-12 11:26:38 +0900

    convolutioal layers

--- tensorflow/mnist/multilayer/testmnist3.py
+++ tensorflow/mnist/multilayer/testmnist3.py
@@ -1,40 +1,90 @@
+# based on https://www.tensorflow.org/get_started/mnist/pros
+
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('../MNIST_data', one_hot=True)
 
 import tensorflow as tf
 sess = tf.InteractiveSession()
 
-# Placeholders
-## The input images x will consist of a 2d tensor of floating point numbers.
-x = tf.placeholder(tf.float32, shape=[None, 784])
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
 
-## The target output classes y_ will also consist of a 2d tensor,
-## where each row is a one-hot 10-dimensional vector indicating which digit class (zero through nine) the corresponding MNIST image belongs to.
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+# Convolution and Pooling
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
+# Placeholders
+x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-# Variables
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-## Before Variables can be used within a session, they must be initialized using that session.
+# First Convolutional Layer
+## first layer will consist of convolution, followed by max pooling
+## The convolution will compute 32 features for each 5x5 patch.
+## Its weight tensor will have a shape of [5, 5, 1, 32].
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+## reshape x to a 4d tensor
+x_image = tf.reshape(x, [-1,28,28,1])
+
+## We then convolve x_image with the weight tensor, add the bias, apply the ReLU function, and finally max pool.
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+## The max_pool_2x2 method will reduce the image size to 14x14.
+h_pool1 = max_pool_2x2(h_conv1)
+
+# Second Convolutional Layer
+## The second layer will have 64 features for each 5x5 patch.
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2) ## the image size will be reduced to 7x7
+
+# Densely Connected Layer
+## Now that the image size has been reduced to 7x7, we add a fully-connected layer with 1024 neurons to allow processing on the entire image.
+## We reshape the tensor from the pooling layer into a batch of vectors, multiply by a weight matrix, add a bias, and apply a ReLU.
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+## Dropout
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# Readout Layer
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+# Train and Evaluate the Model
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
 
-# Predicted Class and Loss Function
-y = tf.matmul(x,W) + b
+for i in range(1000): # 20000
+  batch = mnist.train.next_batch(50)
+  if i%100 == 0:
+    train_accuracy = accuracy.eval(feed_dict={
+        x:batch[0], y_: batch[1], keep_prob: 1.0})
+    print("step %d, training accuracy %g"%(i, train_accuracy))
+  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-## tf.nn.softmax_cross_entropy_with_logits internally applies the softmax on the model's unnormalized model prediction and sums across all classes
-## tf.reduce_mean takes the average over these sums.
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-
-# Train the Model
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-for _ in range(1000):
-  batch = mnist.train.next_batch(100)
-  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-
-# Evaluate the Model
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
