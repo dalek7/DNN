6e28c14a2f5e5f515e722a42cf6b46c6e19b1bff
seung <dalek@kaist.ac.kr>
2017-04-05 09:29:48 +0900

    run example

--- tensorflow/ais/ais_ass1_2.py
+++ tensorflow/ais/ais_ass1_2.py
@@ -2,15 +2,18 @@
 import numpy as np
 import tensorflow as tf
 sess = tf.Session()
-x = tf.placeholder("float", [1, 3])
-w = tf.Variable(tf.random_normal([3, 3]), name='w')
-y = tf.matmul(x, w)
-relu_out = tf.nn.relu(y)
+x           = tf.placeholder("float", [1, 3])
+w           = tf.Variable(tf.random_normal([3, 3]), name='w')
+y           = tf.matmul(x, w)
+relu_out    = tf.nn.relu(y)
 #sess.run(tf.initialize_all_variables())
 sess.run(tf.global_variables_initializer())
 print 'w = '
 print sess.run(w)
 
+print 'x = '
+print x
+print np.array([[1.0, 2.0, 3.0]])
 
 print
 print 'y ='
@@ -18,6 +21,11 @@ print sess.run(y, feed_dict={x:np.array([[1.0, 2.0, 3.0]])})
 print
 print 'relu_out ='
 print sess.run(relu_out, feed_dict={x:np.array([[1.0, 2.0, 3.0]])})
-#print sess.run(relu_out, feed_dict={x:np.array([[1.0, 2.0, 3.0]])})
+
+writer = tf.summary.FileWriter('/tmp/tf_logs/relu1',sess.graph)
+
 
 sess.close()
+
+
+#$ tensorboard --logdir=/tmp/tf_logs/relu1 
