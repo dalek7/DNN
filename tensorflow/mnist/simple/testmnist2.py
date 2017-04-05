fb30f201d40c1568ca8ddfbc4e5502980b6d1e1a
seung <dalek@kaist.ac.kr>
2017-04-05 12:31:21 +0900

    edited accessing weights

--- tensorflow/mnist/simple/testmnist2.py
+++ tensorflow/mnist/simple/testmnist2.py
@@ -9,13 +9,15 @@ Y_train = mnist.train.labels
 X_test = mnist.test.images
 Y_test = mnist.test.labels
 
+print( 'mnist.train.images.size = %d' % X_train.size)
+print( 'mnist.train.labels.size = %d' % Y_train.size)
+
 print X_train.size /28/28, Y_train.size
 
 # Get the next 64 images array and labels
 batch_X, batch_Y = mnist.train.next_batch(64)
-print batch_X.size, batch_Y.size
-print batch_X.size/64, batch_Y.size/64
-print batch_X.size/28/28, batch_Y.size
+#print batch_X.size, batch_Y.size
+print ('batch_X.size/28/28= %d, batch_Y.size/10= %d' % (batch_X.size/28/28, batch_Y.size/10))
 
 
 x = tf.placeholder(tf.float32, [None, 784])
@@ -33,14 +35,24 @@ tf.global_variables_initializer().run()
 for iter in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+  #print('iter %d' % iter)
   if iter % 100 == 0:
-      f = open('w_%s.txt' % iter, 'w')
+      print('out/w_%s.txt' % iter)
+      f = open('out/w_%s.txt' % iter, 'w')
+      W1 = sess.run(W)
+      #print W1.size / 10
+      for i in range(W1.size/10): # inspect w for only '0'
+        f.write("\t".join(map(lambda x: str(x), W1[i])))
+      f.close()
+
+''' This takes too much time !
       for i in range(784):
           Wi = sess.run (W[i])
           #print "\t".join(map(lambda x: str(x), Wi))
           f.write("\t".join(map(lambda x: str(x), Wi)))
           f.write('\n')
-      f.close()
+'''
+
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
